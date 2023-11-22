4e4a10575f0c11c6f89bc4a5ee12ea23a28d58fd
unknown <felipebpassos@gmail.com>
2023-11-22 15:08:13 -0300

    terminado

--- js/descricao-dinamica.js
+++ js/descricao-dinamica.js
@@ -2,12 +2,15 @@ function showDescription(index) {
     let description = document.querySelectorAll('.slides')[index - 1].querySelector('.description');
     let imagem = document.querySelectorAll('.slides')[index - 1].querySelector('img');
 
-    if (window.innerWidth <= 930) {
-        description.style.transform = 'translate(-25%, -50%)';
-        imagem.style.transform = 'translateX(-50%)';
-    } else {
+    if (window.innerWidth >= 930) {
         description.style.transform = 'translate(20%, -50%)'; // ou qualquer valor padrão para telas maiores
         imagem.style.transform = 'translateX(-50%)'; // ou qualquer valor padrão para telas maiores
+    } else if (window.innerWidth <= 500) {
+        description.style.transform = 'translate(-35%, -50%)'; // ou qualquer valor padrão para telas maiores
+        imagem.style.transform = 'scale(0.8) translateX(-43%)'; // ou qualquer valor padrão para telas maiores
+    } else {
+        description.style.transform = 'translate(-25%, -50%)';
+        imagem.style.transform = 'translateX(-50%)';
     }
 
     description.style.opacity = '1';
@@ -17,13 +20,17 @@ function hideDescription(index) {
     let description = document.querySelectorAll('.slides')[index - 1].querySelector('.description');
     let imagem = document.querySelectorAll('.slides')[index - 1].querySelector('img');
 
-    if (window.innerWidth <= 930) {
-        description.style.transform = 'translate(-25%, -50%)';
-        imagem.style.transform = 'translateX(0)';
-    } else {
+    if (window.innerWidth >= 930) {
         description.style.transform = 'translate(-20%, -50%)'; // ou qualquer valor padrão para telas maiores
         imagem.style.transform = 'translateX(0)'; // ou qualquer valor padrão para telas maiores
+    } else if (window.innerWidth <= 500) {
+        description.style.transform = 'translate(-35%, -50%)'; // ou qualquer valor padrão para telas maiores
+        imagem.style.transform = 'scale(0.8)'; // ou qualquer valor padrão para telas maiores
+    } else {
+        description.style.transform = 'translate(-25%, -50%)';
+        imagem.style.transform = 'translateX(0)';
     }
 
     description.style.opacity = '0';
 }
+
